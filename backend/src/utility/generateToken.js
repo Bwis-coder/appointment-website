1ed5c12e3725d6b6abe151c4b134dd723a3a18f3
Bwis <wisdomEzekiel9@gmail.com>
2026-09-24 13:34:54 +0100

    Fix authentication cookie for production

--- backend/src/utility/generateToken.js
+++ backend/src/utility/generateToken.js
@@ -7,12 +7,12 @@ const generateToken = (id, res) => {
     expiresIn: process.env.JWT_EXPIRES_IN || "7d",
   });
 
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: false,
-    sameSite: "lax",
-    maxAge: 60 * 60 * 1000 * 24 * 7,
-  });
+res.cookie("jwt", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 60 * 60 * 1000 * 24 * 7,
+});;
 
   return token;
 };

f2ca4ddbadb807d2f6292ee21433f289e22d2110
Bwis <wisdomEzekiel9@gmail.com>
2026-09-25 18:38:34 +0100

    Match production service setup

--- backend/src/service.js
+++ backend/src/service.js
@@ -1,21 +1,24 @@
 import express from "express";
-import authRoute from "./routes/authRoutes.js";
 import { connectDb, disconnectDb } from "./config/db.js";
-import "dotenv/config";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import authRoute from "./routes/authRoutes.js";
 import doctorRoute from "./routes/doctorRoutes.js";
 import appointmentRoute from "./routes/appointmentRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import { config } from "dotenv";
+config();
 
 connectDb();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5001;
 
-// body parser middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -23,14 +26,27 @@ app.use(
   }),
 );
 
-//Routes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
 app.use("/auth", authRoute);
 app.use("/doctorDetails", doctorRoute);
 app.use("/book", appointmentRoute);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("/{*path}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist", "index.html"));
+  });
+}
 
 process.on("SIGINT", disconnectDb);
 process.on("SIGTERM", disconnectDb);
 
 app.listen(port, () => {
-  console.log(`server running on: ${process.env.BACKEND_URL}`);
+  console.log(`server running on port ${port}`);
 });
